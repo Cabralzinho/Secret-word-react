ff3735426c327c1dd465754510dfe903f29d02ec
Cabral <Cabralzinho@users.noreply.github.com>
2023-06-21 12:46:12 -0300

    First commit in secret word

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Roda", "Volante"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Uva", "Limão"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Cabeça", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "leite", "Ovo"],
+};
